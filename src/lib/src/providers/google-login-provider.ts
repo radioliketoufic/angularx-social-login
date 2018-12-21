23f6009adf748c45881ac373fb42f733b9eaf2fa
Toufic Zayed <toufic_zayed@yahoo.fr>
2018-12-21 10:39:52 +0100

    gapi error init replace platform.js with api.js

--- src/lib/src/providers/google-login-provider.ts
+++ src/lib/src/providers/google-login-provider.ts
@@ -15,7 +15,7 @@ export class GoogleLoginProvider extends BaseLoginProvider {
     initialize(): Promise<void> {
         return new Promise((resolve, reject) => {
             this.loadScript(GoogleLoginProvider.PROVIDER_ID,
-                'https://apis.google.com/js/platform.js',
+                'https://apis.google.com/js/api.js',
                 () => {
                     gapi.load('auth2', () => {
                         this.auth2 = gapi.auth2.init({
